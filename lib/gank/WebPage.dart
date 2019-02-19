cd3401d59023bc20881f95e777aa6fcf8b651147
fujianlian <lianbinqiang@126.com>
2019-02-19 15:18:55 +0800

    webView插件替换

--- lib/gank/WebPage.dart
+++ lib/gank/WebPage.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class WebPage extends StatefulWidget {
   WebPage({Key key, this.url, this.title}) : super(key: key);
@@ -14,43 +14,34 @@ class WebPage extends StatefulWidget {
 }
 
 class WebPageState extends State<WebPage> {
-  var contexts;
-
-  IconData _backIcon() {
-    switch (Theme.of(contexts).platform) {
-      case TargetPlatform.android:
-      case TargetPlatform.fuchsia:
-        return Icons.arrow_back;
-      case TargetPlatform.iOS:
-        return Icons.arrow_back_ios;
-    }
-    assert(false);
-    return null;
-  }
+  var _webViewController;
 
   @override
   Widget build(BuildContext context) {
-    contexts = context;
-    return new MaterialApp(
-      routes: {
-        "/": (_) => new WebviewScaffold(
-              url: widget.url,
-              appBar: new AppBar(
-                title: new Text(widget.title),
-                centerTitle: true,
-                leading: Builder(
-                  builder: (BuildContext context) {
-                    return IconButton(
-                      icon: Icon(_backIcon()),
-                      onPressed: () {
-                        Navigator.pop(contexts);
-                      },
-                    );
-                  },
-                ),
-              ),
-            ),
-      },
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
+        centerTitle: true,
+      ),
+      body: WillPopScope(
+        onWillPop: () {
+          _goBack();
+        },
+        child: WebView(
+          initialUrl: widget.url,
+          onWebViewCreated: (WebViewController webViewController) {
+            _webViewController = webViewController;
+          },
+        ),
+      ),
     );
+  }
+
+  void _goBack() async {
+    if (await _webViewController.canGoBack()) {
+      _webViewController.goBack();
+    } else {
+      Navigator.pop(context);
+    }
   }
 }
